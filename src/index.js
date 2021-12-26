7e912c2a091343c5d2c52cc6bd72361a67fe0859
Rupesh Kumar <111901045@smail.iitpkd.ac.in>
2021-12-27 03:42:58 +0530

    adding and deleting the files to set up the environment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
+// this is the react website for the portfolio of mine 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// calling the render function to render the dom 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+    <h1>this is the starting of the portfolio website </h1>
+  </>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// say everything went fine 
