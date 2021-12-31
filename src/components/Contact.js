16bb57710a76cc619c305ceab708d2b7c0bdb928
Rupesh Kumar <111901045@smail.iitpkd.ac.in>
2021-12-31 09:58:43 +0530

    added the contact section for the website. Now moving to add the footer.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,32 @@ const Contact = () => {
 
     return (
         <>
-            <h1 id="contact-heading">hi this is the contact section for the portfolio website </h1>
+            <h1 id="contact-heading">Contact Me</h1>
+            <div className="contact-container">
+                {/* adding my own details in this section  */}
+                <div className="personal-details">
+                    <div className="phone-details">
+                        <h2>Phone-Number</h2>
+                        <p>8825220213</p>
+                    </div>
+                    <div className="email-details">
+                        <h2>Email-id</h2>
+                        <p>rk763130@gmail.com</p>
+                    </div>
+                </div>
+                {/* now adding section for the user to send message if he/she wants to talk or send some of the query  */}
+                <div className="contact-form">
+                    <form action="">
+                        <div className="formpart1">
+                            <input type="text" placeholder='Name' />
+                            <input type="email" placeholder='Email' />
+
+                        </div>
+                        <textarea name="query" id="query" cols="30" rows="10" placeholder='Tell me your query'></textarea>
+                    </form>
+                    <button type="submit">Send Message</button>
+                </div>
+            </div>
         </>
     )
 }
