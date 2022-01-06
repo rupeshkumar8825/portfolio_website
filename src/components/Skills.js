bc00dc63cfb6615720d6a9a6797f1c5c83d766b2
Rupesh Kumar <111901045@smail.iitpkd.ac.in>
2022-01-06 15:22:16 +0530

    Completed the basic designing of the my skills section. Moving to add the resume section for the portfolio

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,30 +10,42 @@ const Skills = () => {
     return (
         <>
             <div className="skill">
-                <h1 id='skill-heading'>Hi this is the skill section of the portfolio website </h1>
+                <h1 id='skill-heading'>My Skills </h1>
                 <div className="skill-container">
                     <div className="particular-skill">
                         <h2>HTML</h2>
                         <div className="skill-bar" id='html'>
-                            90%
+                            <p>90%</p>
                         </div>
                     </div>
                     <div className="particular-skill" >
                         <h2>CSS</h2>
                         <div className="skill-bar" id='css'>
-                            70%
+                            <p>70%</p>
                         </div>
                     </div>
                     <div className="particular-skill">
                         <h2>JS</h2>
                         <div className="skill-bar" id='js'>
-                            60%
+                            <p>60%</p>
                         </div>
                     </div>
                     <div className="particular-skill">
                         <h2>NodeJs</h2>
                         <div className="skill-bar" id='nodejs'>
-                            50%
+                            <p>50%</p>
+                        </div>
+                    </div>
+                    <div className="particular-skill">
+                        <h2>ReactJs</h2>
+                        <div className="skill-bar" id='reactjs'>
+                            <p>50%</p>
+                        </div>
+                    </div>
+                    <div className="particular-skill">
+                        <h2>MongoDB</h2>
+                        <div className="skill-bar" id='mongodb'>
+                            <p>50%</p>
                         </div>
                     </div>
                 </div>
