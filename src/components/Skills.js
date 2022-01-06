4478d5ab26b6c2f2785f4ace75990331a2a8ab1c
Rupesh Kumar <111901045@smail.iitpkd.ac.in>
2022-01-06 14:16:12 +0530

    done some designing of skill section. still to make the section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,7 +9,35 @@ const Skills = () => {
 
     return (
         <>
-            <h1 id='skill-heading'>Hi this is the skill section of the portfolio website </h1>
+            <div className="skill">
+                <h1 id='skill-heading'>Hi this is the skill section of the portfolio website </h1>
+                <div className="skill-container">
+                    <div className="particular-skill">
+                        <h2>HTML</h2>
+                        <div className="skill-bar" id='html'>
+                            90%
+                        </div>
+                    </div>
+                    <div className="particular-skill" >
+                        <h2>CSS</h2>
+                        <div className="skill-bar" id='css'>
+                            70%
+                        </div>
+                    </div>
+                    <div className="particular-skill">
+                        <h2>JS</h2>
+                        <div className="skill-bar" id='js'>
+                            60%
+                        </div>
+                    </div>
+                    <div className="particular-skill">
+                        <h2>NodeJs</h2>
+                        <div className="skill-bar" id='nodejs'>
+                            50%
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
