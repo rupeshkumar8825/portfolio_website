d47574a73b923367fe88b4d508e807e6568236df
Rupesh Kumar <111901045@smail.iitpkd.ac.in>
2022-01-07 17:20:09 +0530

    Completed the process section. Moving to update the contact section of the portfolio website

--- src/components/Process.js
+++ src/components/Process.js
@@ -11,48 +11,47 @@ const Process = () => {
                 </h1>
                 <div className="process-container">
                     {/* designing the each container using the html  */}
-                    <div className="step-container">
-                        <div className="step-number">
-                            1
-                        </div>
-                        <div className="step-content">
-                            <h1>Get in Touch With us</h1>
-                            <p>Fill the contact form to get in touch with us. Provide little description about the project in the project section in contact form. We will get back to you soon.</p>
+                    <div className="step-container" id='step1'>
+                        <h2>1</h2>
+                        {/* <div className="step-content"> */}
+                        <h1>Get in Touch</h1>
+                        <p>Fill the contact form to get in touch with us. Provide little description about the project in contact form. We will get back to you soon.</p>
 
-                        </div>
+                        {/* </div> */}
                     </div>
 
-                    <div className="step-container">
-                        <div className="step-number">
-                            2
-                        </div>
-                        <div className="step-content">
-                            <h1>Discuss your requirements</h1>
-                            <p>The first step is to get in touch with us and then we will proceed further in this</p>
+                    <div className="step-container" id='step2'>
+                        {/* <div className="step-number"> */}
+                        <h2>2</h2>
+                        {/* </div> */}
+                        {/* <div className="step-content"> */}
+                        <h1>Talk to us</h1>
+                        <p>Discuss with the clients requirements and understand their need what they actually need. We work closely with the clients.</p>
 
-                        </div>
+                        {/* </div> */}
                     </div>
 
-                    <div className="step-container">
-                        <div className="step-number">
-                            3
-                        </div>
-                        <div className="step-content">
-                            <h1>Finalise the business deal</h1>
-                            <p>The first step is to get in touch with us and then we will proceed further in this</p>
+                    <div className="step-container" id='step3'>
+                        {/* <div className="step-number"> */}
+                        <h2>3</h2>
+                        {/* </div> */}
+                        {/* <div className="step-content"> */}
+                        <h1>Finalise the deal</h1>
+                        <p>We finalise the business deal with the client at fixed price and fixed deadline depending of the kind of project.</p>
 
-                        </div>
+                        {/* </div> */}
                     </div>
-                    <div className="step-container">
-                        <div className="step-number">
-                            4
-                        </div>
-                        <div className="step-content">
-                            <h1>Starts working on the project   </h1>
-                            <p>The first step is to get in touch with us and then we will proceed further in this</p>
+                    <div className="step-container" id='step4'>
+                        {/* <div className="step-number"> */}
+                        <h2>4</h2>
+                        {/* </div> */}
+                        {/* <div className="step-content"> */}
+                        <h1>Get started</h1>
+                        <p>Finally ready to start working on the client's projects with full pace. We make sure that we fulfill each and every requirements of the client.</p>
 
-                        </div>
+                        {/* </div> */}
                     </div>
+
                 </div>
 
             </div>
