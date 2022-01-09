a6a48cfbf09e23541bb546062f2cb20fc47dfef4
Rupesh Kumar <111901045@smail.iitpkd.ac.in>
2022-01-09 21:06:04 +0530

    completed the remaining responsive work in work section. Moving forward to host the wesbite on gh pages

--- src/components/Works.js
+++ src/components/Works.js
@@ -17,13 +17,13 @@ const Works = () => {
                     <div className="work-contents">
                         <a href="https://github.com/rupeshkumar8825/todo_combo"><img src={todolist} alt="Todolist" />
                             <h2> Website for Daily Learners</h2>
-                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo rerum beatae voluptas sapiente autem? Non error dolor itaque totam? Eligendi!</p>
+                            <p>This is a simple website for daily learners. This website consists of todolist, notes taking app, calculator and timer. This has full backend as well along with database.</p>
                         </a>
                     </div>
                     <div className="work-contents">
                         <a href="https://rupeshkumar8825.github.io/bootstrap_website/"><img src={blog} alt="Blog website" />
                             <h2>A Simple Blogging Website</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi temporibus vel odio quo odit impedit nam in est quod veniam.</p>
+                            <p>This is a simple bootstrap application. This is a simple blogging website made using bootstrap. This is a multipage responsive application optimised with SEO.</p>
                         </a>
                     </div>
                 </div>
